1789c085cb3c78feeadd172ee977fdda7795ac07
Gowtham M.S. <msgowthamm@gmail.com>
2024-05-14 21:05:15 -0500

    helloworld

---
+++ Angular/HelloWorld/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent // this component is registered to angular
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
